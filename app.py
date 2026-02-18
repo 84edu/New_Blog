53c49a469766b8317eb7c0165eb517d6d39a453a
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-02-18 13:36:58 +0100

    Clean up repository: Remove .idea and add Flask base

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def index():
+    with open("data.json", "r") as fileobj:
+        blog_posts = json.load(fileobj)
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
